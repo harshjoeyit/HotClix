bf94c9a00c3b48b93472725c3a12098d5d63cb6d
harshit gangwar <harshit02gangwar@gmail.com>
2021-01-07 21:31:52 +0530

    Delete keys.js

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-   TOKEN_SECRET: 'assdfadsfasdfadgadgfsdf',
-   AWS_ID: 'AKIAJDKWFCKZCWYSLKHQ',
-   AWS_SECRET: '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',
-   AWS_BUCKET_NAME: 'my-image-gallery',
-}
